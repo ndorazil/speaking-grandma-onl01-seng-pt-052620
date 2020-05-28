e7ded7098b5df8b7ae3811142e3c65232208f3a1
Nathan Dorazil <nathand1915@gmail.com>
2020-05-28 20:31:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@ def speak_to_grandma(phrase)
   if phrase.upcase == 'I LOVE YOU GRANDMA!'
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    "HUH?! SPEAK UP, SONNY!"
   else 
-    puts "HUH?! SPEAK UP, SONNY!"
+    
   end
 end
 
