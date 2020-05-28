bbabef898877370ad226f55193b5474c0f8d97cd
Nathan Dorazil <nathand1915@gmail.com>
2020-05-28 16:59:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,13 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+speak_to_grandma = gets.chomp
+
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+  elsif speak_to_grandma.upcase
+  puts "NO, NOT SINCE 1938!"
+else 
+  puts "HUH?! SPEAK UP, SONNY!"
+end
