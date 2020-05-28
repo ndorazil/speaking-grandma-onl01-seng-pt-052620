3e1d854645d1ab6486c87fa35357eb690ab1144e
Nathan Dorazil <nathand1915@gmail.com>
2020-05-28 17:27:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 speak_to_grandma = gets.chomp
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!".upcase
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak_to_grandma == "".upcase
   puts "NO, NOT SINCE 1938!"
